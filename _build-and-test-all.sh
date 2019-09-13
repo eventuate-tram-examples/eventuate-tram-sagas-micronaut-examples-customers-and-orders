8ca18aa6e56bbeb34c04f972e8d90e967f55a73b
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-09-13 12:22:35 +0300

    Moved docker image version config to gradle

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -5,24 +5,24 @@ set -e
 
 . ./set-env-${DATABASE?}.sh
 
+dockerall="./gradlew ${DATABASE?}Compose"
+dockercdc="./gradlew ${DATABASE?}cdcCompose"
+
 ./gradlew testClasses
 
-docker-compose -f docker-compose-${DATABASE?}.yml down -v
-
-docker-compose -f docker-compose-${DATABASE?}.yml up -d --build zookeeper ${DATABASE?} kafka
-
-./wait-for-${DATABASE?}.sh
-
-docker-compose -f docker-compose-${DATABASE?}.yml up -d --build cdcservice
+${dockerall}Down
+${dockercdc}Build
+${dockercdc}Up
 
 ./wait-for-services.sh $DOCKER_HOST_IP "8099" "actuator/health"
 
 ./gradlew -x :end-to-end-tests:test build
 
-docker-compose -f docker-compose-${DATABASE?}.yml up -d --build 
+${dockerall}Build
+${dockerall}Up
 
 ./wait-for-services.sh $DOCKER_HOST_IP "8081 8082" "health"
 
 ./gradlew :end-to-end-tests:cleanTest :end-to-end-tests:test
 
-docker-compose -f docker-compose-${DATABASE?}.yml down -v
+${dockerall}Down
