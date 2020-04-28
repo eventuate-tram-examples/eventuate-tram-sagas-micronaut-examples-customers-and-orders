1dbf17aa163c2446075d72279e3a11e06a8295ec
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-04-28 14:19:15 +0300

    Simplified docker usage.

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -3,8 +3,6 @@
 set -e
 
 
-. ./set-env-${DATABASE?}.sh
-
 dockerall="./gradlew ${DATABASE?}Compose"
 dockercdc="./gradlew ${DATABASE?}cdcCompose"
 
@@ -14,14 +12,20 @@ ${dockerall}Down
 ${dockercdc}Build
 ${dockercdc}Up
 
-./wait-for-services.sh $DOCKER_HOST_IP "8099" "actuator/health"
-
 ./gradlew -x :end-to-end-tests:test build
+
+#Testing db cli
+if [ "${DATABASE}" == "mysql" ]; then
+  echo 'show databases;' | ./mysql-cli.sh -i
+elif [ "${DATABASE}" == "postgres" ]; then
+  echo '\l' | ./postgres-cli.sh -i
+else
+  echo "Unknown Database"
+  exit 99
+fi
 
 ${dockerall}Build
 ${dockerall}Up
-
-./wait-for-services.sh $DOCKER_HOST_IP "8081 8082" "health"
 
 ./gradlew :end-to-end-tests:cleanTest :end-to-end-tests:test
 
--- build-and-test-all-postgres.sh
+++ build-and-test-all-postgres.sh
@@ -4,4 +4,6 @@ set -e
 
 export DATABASE=postgres
 
+export MICRONAUT_ENVIRONMENTS=postgres
+
 ./_build-and-test-all.sh
